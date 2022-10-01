6016df95a550cc1af15163982ccc5f306466771d
Krefit <gustavokrawczyk@gmail.com>
2022-09-30 21:21:13 -0300

    atualização do projeto

--- src/main/java/Listas/Atividade.java
+++ src/main/java/Listas/Atividade.java
@@ -16,14 +16,22 @@ public class Atividade {
     public static void main(String[] args) throws EmptyListException {
         int valorInsereir;
         int saida = 1;
-        Fila fila = new Fila();
+        
+        
+        
         while (saida != 0) {
             saida = menu();
             switch (saida) {
                 case 1:
+                    Fila fila = new Fila();
                     interacaoFila(1, fila);
-
                     break;
+                case 2:
+                    Pilha pilha = new Pilha();
+                    interacaoPilha(1,pilha);
+                case 3:
+                    Lista lista = new Lista();
+                    interacaoLista(1,lista);
                 default:
                     throw new AssertionError();
             }
@@ -85,6 +93,61 @@ public class Atividade {
                 break;
             case 2:
                 interacaoFila(2, fila);
+                break;
+            case 0:
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    private static void interacaoPilha(int i, Pilha pilha) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void interacaoLista(int parametro, Lista lista) throws EmptyListException {
+        Scanner sc = new Scanner(System.in);
+        int menu;
+        int valorInserir;
+        if (lista.estaVazia()) {
+            System.out.println("Fila ainda está vazia!");
+            System.out.println("Inserir novo elemento");
+            valorInserir = sc.nextInt();
+            
+            fimMenuLista(lista);
+
+        } else if (parametro == 1) {
+            System.out.println("Inserir novo elemento:");
+            valorInserir = sc.nextInt();
+            lista.insereNoInicio(valorInserir);
+            fimMenuLista(lista);
+
+        } else if (parametro == 2) {
+            System.out.println("Desenfilerando fila!");
+            
+            fimMenuLista(lista);
+        }
+    }
+
+    private static void fimMenuLista(Lista lista) throws EmptyListException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Fila atual:");
+        lista.print();
+
+        System.out.println("1 - Inserir novo elemento no inicio da lista");
+        System.out.println("2 - Remover elemento da fila");
+        System.out.println("0 - Sair");
+
+        int menu = sc.nextInt();
+        switch (menu) {
+            case 1:
+                interacaoLista(1, lista);
+                break;
+            case 2:
+                interacaoLista(2, lista);
                 break;
             case 0:
                 break;
